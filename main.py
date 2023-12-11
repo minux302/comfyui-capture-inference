f757393ea55df719d5f4724e220f5df7efd16890
k-azuma-radius5 <k_azuma@radius5.co.jp>
2023-12-12 02:31:40 +0900

    run mac

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import json
 import multiprocessing
-from multiprocessing import Queue, Value, Array
-from client import Client
-from workflow_manager import GenerateSettings, WorkflowManager
-from ui import ui
+from multiprocessing import Array, Queue, Value
+
 from capture_bbox_manager import CaptureBBoxManager
+from client import Client
+from ui import ui
+from workflow_manager import GenerateSettings, WorkflowManager
 
 
 def set_bbox(queue, is_captureing, bbox):
@@ -18,33 +19,34 @@ def set_bbox(queue, is_captureing, bbox):
     queue.put("done")
 
 
-with open("config.json", "r") as f:
-    config = json.load(f)
-workflow_manager = WorkflowManager(f"workflows/{config['init_workflow']}")
-generate_settings = GenerateSettings(**config)
-client = Client()
-bbox_manager = CaptureBBoxManager()  # must be main process, since uses tkinter
-
-# status shared between processes
-queue = Queue()
-is_capturing = Value('i', 0)
-bbox = Array('i', [0, 0, 0, 0])
-
-ui_process = multiprocessing.Process(
-    target=ui,
-    args=(
-        config,
-        workflow_manager,
-        generate_settings,
-        client,
-        queue,
-        is_capturing,
-        bbox
-    ),
-)
-ui_process.start()
-
-while True:
-    message = queue.get()
-    if message == "set_bbox":
-        set_bbox(queue, is_capturing, bbox)
+if __name__ == "__main__":
+    with open("config.json", "r") as f:
+        config = json.load(f)
+    workflow_manager = WorkflowManager(f"workflows/{config['init_workflow']}")
+    generate_settings = GenerateSettings(**config)
+    client = Client()
+    bbox_manager = CaptureBBoxManager()  # must be main process, since uses tkinter
+    
+    # status shared between processes
+    queue = Queue()
+    is_capturing = Value('i', 0)
+    bbox = Array('i', [0, 0, 0, 0])
+    
+    ui_process = multiprocessing.Process(
+        target=ui,
+        args=(
+            config,
+            workflow_manager,
+            generate_settings,
+            client,
+            queue,
+            is_capturing,
+            bbox
+        ),
+    )
+    ui_process.start()
+    
+    while True:
+        message = queue.get()
+        if message == "set_bbox":
+            set_bbox(queue, is_capturing, bbox)
